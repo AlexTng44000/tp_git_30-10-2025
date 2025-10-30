69cdeb08acac45d88a943336ef08bfadb25e1c18
Alexandre TANGUY <alexandretang17@gmail.com>
2025-10-30 12:26:09 +0100

    fix: migrate env to globals for ESLint 9 flat config

--- eslint.config.js
+++ eslint.config.js
@@ -3,20 +3,27 @@ import pluginImport from "eslint-plugin-import";
 import prettierConfig from "eslint-config-prettier";
 
 export default [
-  js.configs.recommended,
-  prettierConfig,
-  {
-    plugins: {
-      import: pluginImport,
+    js.configs.recommended,
+    prettierConfig,
+    {
+        plugins: {
+            import: pluginImport,
+        },
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                process: "readonly",
+                module: "readonly",
+                require: "readonly",
+                console: "readonly",
+                __dirname: "readonly",
+                jest: "readonly",
+            },
+        },
+        rules: {
+            "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+            "import/no-unresolved": "off",
+        },
     },
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    env: { node: true, es2022: true, jest: true },
-    rules: {
-      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-      "import/no-unresolved": "off",
-    },
-  },
 ];
